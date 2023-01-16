f47a88d8f0353bfec4261b0f65ec9be862b019b2
Agustín García <agustin.tavoite@gmail.com>
2023-01-16 11:43:32 +0100

    .

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,7 +6,6 @@ import { LoginContainer, LoginButton } from "./Styles"
 
 
 const Login = () => {
-
   return (
     <LoginContainer>
       <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" alt="logo" className="logo" />
